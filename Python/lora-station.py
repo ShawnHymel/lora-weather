459a7ac8b7885cca10681c3d08ada613cd34a5bf
ShawnHymel <hymelsr@vt.edu>
2019-03-24 10:12:15 -0500

    Added Python weather station

---
+++ Python/lora-station.py
@@ -0,0 +1,41 @@
+import time
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+import adafruit_rfm9x
+
+# Configure RFM9x LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+# Attempt to set up the RFM9x module
+try:
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+    print('RFM9x detected')
+except RuntimeError:
+    print('RFM9x error')
+
+
+# Wait for LoRa packets
+while True:
+    packet = None
+    packet = rfm9x.receive()
+    if packet != None:
+        
+        print("Received:",  str(packet))
+        
+        # Split packet
+        from_addr = packet[0]
+        to_addr = packet[1]
+        temp = int.from_bytes(packet[2:4], byteorder='little') / 10.0
+        humd = int.from_bytes(packet[4:6], byteorder='little') / 10.0
+        pres = int.from_bytes(packet[6:8], byteorder='little') / 10.0
+
+        # Print results
+        print("From:", from_addr)
+        print("To:", to_addr)
+        print("Temperature:", temp, "C")
+        print("Humidity:", humd, "%")
+        print("Pressure:", pres, "hPa")
+        print()
